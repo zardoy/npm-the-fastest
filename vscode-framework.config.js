6784f3f0387e4504d2f188795db66bc9db21b211
Vitaly Turovsky <vital2580@icloud.com>
2021-10-22 23:15:34 +0300

    reveal algolia search

--- vscode-framework.config.js
+++ vscode-framework.config.js
@@ -1,7 +1,15 @@
+const dotenv = require('dotenv')
+
+dotenv.config({ path: '.env.local' })
+
 /** @type{import('vscode-framework/build/config').UserConfig*/
 const config = {
     esbuildConfig: {
         external: ['@vue/compiler-sfc'],
+        define: {
+            ALGOLIA_APP_ID: process.env.ALGOLIA_APP_ID,
+            ALGOLIA_APP_KEY: process.env.ALGOLIA_APP_KEY,
+        },
     },
 }
 
