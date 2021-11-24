6854c28df67f5cc3a6718e1af625a83f11421506
Vitaly Turovsky <vital2580@icloud.com>
2021-11-24 09:12:40 +0300

    feat(open-closest-package-json-command): open root package json if no active editor

--- src/commands/openClosestPackageJson.ts
+++ src/commands/openClosestPackageJson.ts
@@ -1,10 +1,20 @@
+import { existsSync } from 'fs'
+import { join } from 'path'
 import vscode from 'vscode'
 import { Utils } from 'vscode-uri'
 import { findUpPackageJson } from '../commands-core/packageJson'
+import { getCurrentWorkspaceRoot } from '../commands-core/util'
 
 export const openClosestPackageJson = async () => {
     const uri = vscode.window.activeTextEditor?.document.uri
-    if (uri === undefined) return
+    if (uri === undefined || uri.scheme === 'untitled') {
+        const firstWorkspace = vscode.workspace.workspaceFolders?.[0]
+        if (!firstWorkspace || !existsSync(join(firstWorkspace.uri.fsPath, 'package.json'))) return
+        const uriToOpen = Utils.joinPath(firstWorkspace.uri, 'package.json')
+        await vscode.window.showTextDocument(uriToOpen)
+        return getCurrentWorkspaceRoot()
+    }
+
     const closestPackageJson = await findUpPackageJson(uri)
     if (closestPackageJson === undefined) {
         await vscode.window.showWarningMessage('No closest package.json found')
