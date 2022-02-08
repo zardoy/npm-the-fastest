befc46c7f2fa2d533e6db903fcba98b86c7abd6c
Vitaly Turovsky <vital2580@icloud.com>
2022-02-08 03:17:00 +0300

    feat: **Start script command**: Add item button for revealing the script contents in package.json

--- src/commands/startNpmScript.ts
+++ src/commands/startNpmScript.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode'
+import { parseTree, findNodeAtLocation } from 'jsonc-parser'
 import { getExtensionSetting, showQuickPick, VSCodeQuickPickItem } from 'vscode-framework'
 import { move } from 'rambda'
 import { launchNpmTask } from '../commands-core/npmScripts'
@@ -7,7 +8,7 @@ const lastTouchedScripts = new Set<string>()
 
 export const startNpmScript = async () => {
     // const name = await vscode.window.showErrorMessage('No `scripts` defined in package.json', 'Open package.json')
-    await launchNpmTask(async ({ packageJson }) => {
+    await launchNpmTask(async ({ packageJson, dir }) => {
         const scriptNamespaceIcon = {
             test: 'test-view-icon',
             generate: 'code',
@@ -16,20 +17,42 @@ export const startNpmScript = async () => {
             start: 'play-circle',
         }
         const getIconForScript = (scriptName: string) => {
-            for (const [detector, icon] of Object.entries(scriptNamespaceIcon)) if (scriptName.includes(detector)) return ` $(${icon})`
+            for (const [detector, icon] of Object.entries(scriptNamespaceIcon)) if (scriptName.includes(detector)) return `$(${icon})`
 
             return ''
         }
 
         const runningNpmTasks = vscode.tasks.taskExecutions.filter(({ task }) => task.source === 'npm')
+        const quickPick = vscode.window.createQuickPick<VSCodeQuickPickItem>()
+        type ItemButtons = Array<
+            vscode.QuickInputButton & {
+                click(item)
+            }
+        >
         let picks = Object.entries(packageJson.scripts || {}).map(([scriptName, contents]): VSCodeQuickPickItem => {
             let label = `${getIconForScript(scriptName)} ${scriptName}`
             if (runningNpmTasks.some(({ task }) => task.name === scriptName)) label = `$(loading~spin)${label}`
+
+            const itemButtons: ItemButtons = [
+                {
+                    iconPath: new vscode.ThemeIcon('go-to-file'),
+                    tooltip: 'Reveal script in package.json',
+                    async click() {
+                        const packageJsonUri = vscode.Uri.joinPath(dir, 'package.json')
+                        const editor = await vscode.window.showTextDocument(packageJsonUri)
+                        const { document } = editor
+                        const { offset, length } = findNodeAtLocation(parseTree(document.getText())!, ['scripts', scriptName])!
+                        editor.selection = new vscode.Selection(document.positionAt(offset + 1), document.positionAt(offset + 1 + length - 2))
+                        editor.revealRange(editor.selection)
+                    },
+                },
+            ]
             return {
                 label,
                 value: scriptName,
                 detail: contents,
                 description: '',
+                buttons: itemButtons,
             }
         })
         for (const lastTouchedScript of lastTouchedScripts.keys()) {
@@ -38,8 +61,28 @@ export const startNpmScript = async () => {
             picks = move(pickIndex, 0, picks)
         }
 
-        const npmScript = await showQuickPick(picks, {
-            matchOnDetail: getExtensionSetting('scripts.matchContents'),
+        quickPick.items = picks
+        quickPick.matchOnDetail = getExtensionSetting('scripts.matchContents')
+        quickPick.onDidTriggerItemButton(({ item, button: itemButton }) => {
+            for (const button of item.buttons! as ItemButtons) {
+                if (button !== itemButton) return
+                button.click(item)
+            }
+        })
+        // copied from vscode-extra
+        const npmScript = await new Promise<string | undefined>(resolve => {
+            quickPick.onDidHide(() => {
+                resolve(undefined)
+                quickPick.dispose()
+            })
+            quickPick.onDidAccept(() => {
+                // align with default showQuickPick behavior
+                if (quickPick.items.length === 0) return
+                const { selectedItems } = quickPick
+                resolve(selectedItems[0]?.value)
+                quickPick.hide()
+            })
+            quickPick.show()
         })
         if (npmScript === undefined) return
         lastTouchedScripts.add(npmScript)
